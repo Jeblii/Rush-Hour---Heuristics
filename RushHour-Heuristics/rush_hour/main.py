5209eab772cd8ce0089a590d359c0c199085eb7e
Jeffrey Tsang <jtsang93@gmail.com>
2018-01-18 14:50:29 +0100

    .

--- RushHour-Heuristics/rush_hour/main.py
+++ RushHour-Heuristics/rush_hour/main.py
@@ -21,7 +21,7 @@ def create_board(txt, size):
     new_board = board(vehicles)
     return new_board
 
-mylist = load_text("70") #runs the setup file
+mylist = load_text("4000") #runs the setup file
 size = len(mylist) #reads the number of lines of mylist
 init_board = create_board(mylist, size) #puts all the vehicles into a generated board
 
