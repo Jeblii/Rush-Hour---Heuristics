6a21ca3ecb31c99b0d78717f077b777e7affb900
Rouel de Romas <rouel57@gmail.com>
2018-01-18 14:52:05 +0100

    blab

--- RushHour-Heuristics/rush_hour/Algorithms/BreadthFirstSearch.py
+++ RushHour-Heuristics/rush_hour/Algorithms/BreadthFirstSearch.py
@@ -20,21 +20,9 @@ def breadthfirstsearch(board, maxDepth=100):
         for individual in range(len(queue)):
             print("depth is", depth + 1)
             print("checking:", individual ,"/", len(queue),"\n")
+
             next_board = queue[individual]
-
-            #visit_count += 1                                                #number of visited states in the breadthfirstsearch
-            #visit[visit_count] = queue[individual].get_board()              #create key for a visited state in visit dictionary
-
             new_gen = next_board.calculate_next_move()
-                                                                     #save the states of new_gen that are already in visit dictionary
-            # for i in new_gen:
-            #     for o in visit.values():
-            #         if np.array_equal(i.get_board(), o) == True:
-            #             ng.append(i)
-            # for i in ng:                                                    #remove the visited states from new_gen -> will not show a visited state as child
-            #     if i in new_gen:
-            #         new_gen.remove(i)
-
 
             for child in new_gen:
                 new_key = child.get_board()
@@ -47,7 +35,7 @@ def breadthfirstsearch(board, maxDepth=100):
                         visit[len(visit)+1] = child.get_board()                 #add last visited state to visit dictionary
 
                         print("Total visited states:",len(visit))
-                        # for i in visit.values():                            #prints all the visited states in breatdhfirst search
+                        # for i in visit.keys():                            #prints all the visited states in breatdhfirst search
                         #     print(i)                                        #<- uncomment this section if you want to seethe visited states in the breadthfirstsearch
 
                         print("\nReversed solution route")
