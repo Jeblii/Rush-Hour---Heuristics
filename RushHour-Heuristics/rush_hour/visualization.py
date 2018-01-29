3203b0580b93a2a4c2829e005cab213fdfee5c23
Bas de Jong <bassdejongg@gmail.com>
2018-01-29 12:04:42 +0100

    'visualization'

--- RushHour-Heuristics/rush_hour/visualization.py
+++ RushHour-Heuristics/rush_hour/visualization.py
@@ -4,15 +4,7 @@ import numpy as np
 import time
 
 class Visualization(object):
-    def __init__(self, steps):
-        self.steps = steps
-        # self.win = win
-
-        path = self.steps[0]
-        win = self.steps[1]
-        print(path)
-        w = np.array(win)
-        print(w)
+    def __init__(self, winning_state):
 
         #initializes board
         pg.init()
@@ -26,38 +18,31 @@ class Visualization(object):
 
         #loads Background Image
         board = pg.image.load('Blocks/Background.jpg')
-        red_car = pg.image.load('Blocks/Car0.png')
 
         #Initializes Background Image
         DISPLAYSURF.blit(board, (0,0))
 
-        #Load cars into board
-        car_0 = np.where(win == '0.')
-        print (car_0)
-        for i in car_0[0]:
-            y = (dif_heigth * i) + 25
-
-        for i in car_0[1]:
-            x = (dif_width *i) - 100
-        print (y)
-        print(x)
-
-        DISPLAYSURF.blit(red_car, (x,y))
+        solution_steps = [winning_state]
+        parent = winning_state.parent
+        while parent:
+            solution_steps.append(parent)
+            parent = parent.parent
+        solution_steps = reversed(solution_steps)
 
         colorcoding = []
         imagelist = []
 
-        for number in range(0,16):
-            imagelist.append(pg.image.load('Blocks/' + "Car" + number + ".png"))
-        for number in range(1,16):
-            imagelist.append(pg.image.load('Blocks/' + "Car-rotated" + number + ".png"))
+        for number in range(0, 16):
+            imagelist.append(pg.image.load('Blocks/' + "Car" + str(number) + ".png"))
+        for number in range(1, 16):
+            imagelist.append(pg.image.load('Blocks/' + "Car-rotated" + str(number) + ".png"))
 
         horizontalcars = imagelist[0:12]
         horizontaltrucks = imagelist[12:16]
         verticalcars = imagelist[16:25]
         verticaltrucks = imagelist[25:]
 
-        for vehicle in board.vehicles:
+        for vehicle in winning_state.vehicles:
             if vehicle.orientation == 1:
                 if vehicle.id < 12:
                     colorcoding.append(horizontalcars[0])
@@ -77,71 +62,33 @@ class Visualization(object):
                     verticaltrucks.append(verticaltrucks[0])
                     verticaltrucks.pop(0)
 
+        counter = 0
+        max = (len(solution_steps))
+
         while True:
-            for state in winning_route:
-                clean board init
-                for vehicle in range(len(state.vehicles)):
-                    x = (dif_width * state.vehicles[vehicle].x) - 100
-                    y = (dif_heigth * state.vehicles[vehicle].y) + 25
-                    DISPLAYSURF.blit(colorcoding[vehicle], (x, y))
-                pg.display.update
-                time.sleep(.5)
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     pg.quit()
                     sys.exit()
-                pg.display.update()
-
-
-
-def visualize(self, winning_state):
-
-    solution_steps = [winning_state]
-    parent = winning_state.parent
-    while parent:
-        solution_steps.append(parent)
-        parent = parent.parent
-    solution_steps = reversed(solution_steps)
-
-    colorcoding = []
-    imagelist = []
-
-    for number in range(0,16):
-        imagelist.append(pg.image.load('Blocks/' + "Car" + str(number) + ".png"))
-    for number in range(1,16):
-        imagelist.append(pg.image.load('Blocks/' + "Car-rotated" + str(number) + ".png"))
-
-    horizontalcars = imagelist[0:12]
-    horizontaltrucks = imagelist[12:16]
-    verticalcars = imagelist[16:25]
-    verticaltrucks = imagelist[25:]
-
-    for vehicle in winning_state.vehicles:
-        if vehicle.orientation == 1:
-            if vehicle.id < 12:
-                colorcoding.append(horizontalcars[0])
-                horizontalcars.append(horizontalcars[0])
-                horizontalcars.pop(0)
-            else:
-                colorcoding.append(horizontaltrucks[0])
-                horizontaltrucks.append(horizontaltrucks[0])
-                horizontaltrucks.pop(0)
-        else:
-            if vehicle.id < 12:
-                colorcoding.append(verticalcars[0])
-                verticalcars.append(verticalcars[0])
-                verticalcars.pop(0)
-            else:
-                colorcoding.append(verticaltrucks[0])
-                verticaltrucks.append(verticaltrucks[0])
-                verticaltrucks.pop(0)
-
-    for state in solution_steps:
-        #clean board init
-        for vehicle in range(len(state.vehicles)):
-            x = (dif_width * state.vehicles[vehicle].x) - 100
-            y = (dif_heigth * state.vehicles[vehicle].y) + 25
-            DISPLAYSURF.blit(colorcoding[vehicle], (x, y))
-        pg.display.update
-        time.sleep(.5)
-
+                    pg.display.update()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        if counter > 0:
+                            DISPLAYSURF.blit(board, (0, 0))
+                            state = solution_steps[counter]
+                            for vehicle in range(len(state.vehicles)):
+                                x = (dif_width * state.vehicles[vehicle].x) - 100
+                                y = (dif_heigth * state.vehicles[vehicle].y) + 25
+                                DISPLAYSURF.blit(colorcoding[vehicle], (x, y))
+                            counter -=1
+                            pg.display.update
+                if event.key == pygame.K_RIGHT:
+                    if counter < max:
+                        DISPLAYSURF.blit(board, (0, 0))
+                        state = solution_steps[counter]
+                        for vehicle in range(len(state.vehicles)):
+                            x = (dif_width * state.vehicles[vehicle].x) - 100
+                            y = (dif_heigth * state.vehicles[vehicle].y) + 25
+                            DISPLAYSURF.blit(colorcoding[vehicle], (x, y))
+                        counter += 1
+                        pg.display.update
